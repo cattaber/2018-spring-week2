9eae58ac4d54ecb0a7bf0fd2d23bcf861c43fdff
Catherine <catherine.e.taber@gmail.com>
2018-03-10 09:43:11 -0600

    Add week 2 Javascript

---
+++ index.js
@@ -0,0 +1,29 @@
+
+var beers = 99;
+while(beers > 0) {
+
+
+
+console.log (beers + " bottles of beer on the wall");
+console.log (beers + " bottles of beer");
+console.log ("take one down, pass it around");
+beers--;
+console.log ( beers + " bottles of beer on the wall\n");
+}
+//beers = beers-1;
+
+
+// if (beers ===1) {
+// console.log (beers + " bottle of beer on the wall");
+// console.log (beers + " bottle of beer");
+// console.log ("take one down, pass it around");
+// beers--;
+// console.log ( beers + " bottles of beer on the wall");
+
+// } else {
+// console.log (beers + " bottles of beer on the wall");
+// console.log (beers + " bottles of beer");
+// console.log ("take one down, pass it around");
+// beers--;
+// console.log ( beers + " bottles of beer on the wall");
+// }
